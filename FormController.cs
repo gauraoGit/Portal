d1d1a995f7bc7d3bf9bc4b331f2bc9e2bbbe77ff
gauraoGit <gauraotarpe@gmail.com>
2016-10-11 22:36:02 -0500

    Convert CSV to datatable

---
+++ FormController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCKendoUIGrid.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(FormCollection fc, HttpPostedFileBase fileInput)
+        {
+            Stream data= fileInput.InputStream;
+            DataTable dt= ConvertCSVtoDataTable(data);
+            ViewBag.status = "Update Successfull";
+            return View("Index", ViewBag);
+        }
+
+        public static DataTable ConvertCSVtoDataTable(Stream stream)
+        {
+            StreamReader sr = new StreamReader(stream);
+            string[] headers = sr.ReadLine().Split(',');
+            DataTable dt = new DataTable();
+            foreach (string header in headers)
+            {
+                dt.Columns.Add(header);
+            }
+            while (!
+                sr.EndOfStream)
+            {
+                string[] rows = Regex.Split(sr.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                DataRow dr = dt.NewRow();
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    dr[i] = rows[i];
+                }
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
+    }
+}
